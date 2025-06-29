157fd52ca48598db998bd9b4702d7dc2acc07b0e
fmgermano <fmgermano@gmail.com>
2025-06-28 21:11:22 -0300

    fix: error 404

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/felipe-mateus-studio/' : '/', // ESSENCIAL
   server: {
     host: "::",
     port: 8080,
   },
-  base: mode === 'production' ? '/felipe-mateus-studio/' : '/', // ⚠️ esta linha é chave
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
